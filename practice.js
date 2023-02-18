b1dda2b158b3728bc2d37de8b8e497867bb7746d
MolchanovSergii <molchanse@gmail.com>
2023-02-18 15:15:57 +0200

    task 2

--- practice.js
+++ practice.js
@@ -9,11 +9,34 @@
 
 
 
- const officialName = prompt("Яка офіційна назва JavaScript?")
+//  const officialName = prompt("Яка офіційна назва JavaScript?")
 
- if(officialName === "ECMAScript") {
-    alert('Вірно')
- } else {
-    alert("Не знаєте? ECMAScript!")
+//  if(officialName === "ECMAScript") {
+//     alert('Вірно')
+//  } else {
+//     alert("Не знаєте? ECMAScript!")
 
- }
+//  }
+
+ 
+/**
+ *? Напишіть програму, яка отримає від користувача
+ *? число (кількість хвилин) та виведе в консоль
+ *? рядок у форматі годин та хвилин
+ *? https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ *? 70 === 01:10
+ */
+function takeTime(number) {
+    
+
+    const hour = String(Math.floor(number / 60)).padStart(2,0);
+    const min = String(number % 60).padStart(2,0);
+    
+    console.log(`${hour}:${min}`);
+
+}
+
+const askMinutes = prompt("pls enter minutes");
+
+takeTime(askMinutes);
+
