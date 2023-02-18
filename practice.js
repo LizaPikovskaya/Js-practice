d9b4c0164128587035f0078d65f6e9311695e162
nazario17 <hazarkorchak@gmail.com>
2023-02-18 16:08:11 +0200

    task-4

--- practice.js
+++ practice.js
@@ -49,15 +49,42 @@
  */
 
 
-function numberDecrease(min, max) { 
-    for (let i = max; i >= min; i -= 1) { console.log(i) } 
-    for (let i = min; i <= max; i += 1) { 
-        if (i % 2 === 0) { 
-            console.log(i)
-        } 
-    }
-}
-numberDecrease(1, 10)
+// function numberDecrease(min, max) {
+//     for (let i = max; i >= min; i -= 1) { console.log(i) }
+//     for (let i = min; i <= max; i += 1) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// numberDecrease(1, 10)
     
+
+/**
+ *? При завантаженні сторінки користувачеві пропонується
+ *? в prompt ввести число. Введення додається до значення
+ *? змінної total.
+ *? Операція введення числа триває до того часу,
+ *? поки користувач не натисне кнопку Cancel у prompt.
+ *? Після того як користувач припинив введення, натиснувши на
+ *? кнопку Cancel, показати alert з рядком "Загальна сума введених чисел дорівнює [число]."
+ 
+ *! Робити перевірку, що користувач ввів саме число,
+ *! а не довільний набір символів не потрібно.
+ */
+
+
+let total = 0
+let a = prompt("Enter a number"); 
+
+ do {
+
+     total += Number(a)
+     a = prompt("Enter a number");
+
+     
+} while (a !== null);
+ 
+console.log(total);
 
 
