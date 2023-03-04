2f8def0234b3ba781820535295b04e8365d531e6
Liza <lizapikovskaya@gmail.com>
2023-03-04 14:32:24 +0200

    do task-7

--- practice.js
+++ practice.js
@@ -115,9 +115,38 @@
 
 
 
- let number = prompt('Введіть число')
+//  let number = prompt('Введіть число')
 
- while(number < 100 && number !== null){
-number = prompt("Введіть число більше 100")
+//  while(number < 100 && number !== null){
+// number = prompt("Введіть число більше 100")
 
- } 
+//  } 
+
+
+/**
+ *? Напиши скрипт для об'єкта user,
+ *? послідовно:
+ *? 1 додати поле mood зі значенням 'happy'
+ *? 2 замінить hobby на 'skydiving'
+ *? 3 замінить значення premium на false
+ *? 4 виводить вміст об'єкта users у форматі
+ *? ключ: значення використовуючи Object.keys() і for...of
+ */
+
+ const user = {
+    name: "John",
+    age: 20,
+    hobby: "tennis",
+    premium: true,
+  };
+
+  user.mood = 'happy'
+  user.hobby = 'skydiving'
+  user.premium = false
+
+
+  const keys = Object.keys(user) 
+  for (const key of keys) {
+    console.log(`${key} : ${user[key]}`);
+  }
+
