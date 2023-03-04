f520c024a7a8911a020ed32016e475d1388dc63e
Liza <lizapikovskaya@gmail.com>
2023-03-04 16:33:27 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -234,3 +234,34 @@
 //     return acc;
 // }, [])
 // console.log(newFruit)
+
+
+/**
+*? Поверніть об'єкт, в якому вказано кількість тегів.
+*? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
+
+ */
+const tweets = [
+    { id: "000", likes: 5, tags: ["js", "nodejs"] },
+    { id: "001", likes: 2, tags: ["html", "css"] },
+    { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+    { id: "003", likes: 8, tags: ["css", "react"] },
+    { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+  ];
+
+
+
+
+  function countTags (array) {
+    const allTags = array.flatMap(obj => obj.tags)
+    const result =  allTags.reduce((acc, item) => {
+         return {...acc,
+            [item]: acc[item] ? acc[item] += 1 : 1
+        }
+    },{})
+
+return result
+  }
+
+
+  console.log(countTags (tweets));
