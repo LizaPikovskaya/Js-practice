b7384080efff2dd243d07627e57522bcb4ebb1d9
Youneri <118117711+Youneri@users.noreply.github.com>
2023-03-04 16:59:18 +0200

    task 12

--- practice.js
+++ practice.js
@@ -241,27 +241,95 @@
 *? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
 
  */
-const tweets = [
-    { id: "000", likes: 5, tags: ["js", "nodejs"] },
-    { id: "001", likes: 2, tags: ["html", "css"] },
-    { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-    { id: "003", likes: 8, tags: ["css", "react"] },
-    { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-  ];
+// const tweets = [
+//     { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//     { id: "001", likes: 2, tags: ["html", "css"] },
+//     { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//     { id: "003", likes: 8, tags: ["css", "react"] },
+//     { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+//   ];
 
 
 
 
-  function countTags (array) {
-    const allTags = array.flatMap(obj => obj.tags)
-    const result =  allTags.reduce((acc, item) => {
-         return {...acc,
-            [item]: acc[item] ? acc[item] += 1 : 1
-        }
-    },{})
+//   function countTags (array) {
+//     const allTags = array.flatMap(obj => obj.tags)
+//     const result =  allTags.reduce((acc, item) => {
+//          return {...acc,
+//             [item]: acc[item] ? acc[item] += 1 : 1
+//         }
+//     },{})
 
-return result
-  }
+// return result
+//   }
 
 
-  console.log(countTags (tweets));
+//   console.log(countTags (tweets));
+
+
+
+
+
+//            HOMEWORK /////////
+
+// const arr = [
+// { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+// { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+// { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+// { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+// { name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
+// { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
+// { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
+// { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
+// { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
+// { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
+// { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
+// { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
+// { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 }
+// ];
+
+// - Получить массив ученых, родившихся в 19 веке.
+// - Найти общую сумму лет, сколько прожили все ученые.
+// - Отсортировать ученых по алфавиту.
+// - Отсортировать ученых по количеству прожитых лет.
+// - Удалить из массива ученых, родившихся в 15, 16 или 17 веках.
+// - Найти ученого, который родился позже всех.
+// - Найти год рождения Albert Einstein.
+// - Найти ученых, фамилия которых начинается на букву "С".
+// - Удалить из массива всех ученых, имя которых начинается на букву "A".
+// - Найти ученого, который прожил больше всех и ученого, который меньше.
+// - Найти ученых, у которых совпадают первые буквы имени и фамилии.
+// - Узнать, все ли ученые работали в 19 веке.
+
+
+
+
+/**
+ *? Из объекта concerts нужно получить массив
+ *? в котором будут только имена городов.
+ *? Из массива нужно убрать города в которых концерт уже прошёл и
+ *? отсортировать их в хронологичном порядке.
+ *? Результат вывести в консоль.
+ *? Ожидаемый результат ["Умань", "Харків", "Одеса"]
+ */
+
+const concerts = {
+  Київ: new Date("2020-04-01"),
+  Умань: new Date("2023-07-02"),
+  Вінниця: new Date("2020-04-21"),
+  Одеса: new Date("2023-07-15"),
+  Хмельницький: new Date("2020-04-18"),
+  Харків: new Date("2023-07-10"),
+};
+
+function concertsDone(object) {
+  let newArr = Object.keys(object);
+  // const newObj = { ...object };
+  return newArr.filter(keys => object[keys] > Date.now())
+    .sort((a, b) => object[a] - object[b]);
+
+
+  
+}
+
+console.log(concertsDone(concerts));
